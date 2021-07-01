392b0200897568c9439f4ff961bed4664ea8d0a1
vinod Dangi <vinoddangi@gmail.com>
2021-07-01 12:55:15 +0530

    test

--- release.config.js
+++ release.config.js
@@ -1,9 +1,33 @@
-module.export = {
-    branches: ["master", "next", "main"],
+module.exports = {
+    branches: ["master", "next", "main", { name: 'beta', prerelease: true }],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/npm",
-        "@semantic-release/git"
-    ]
+        "@semantic-release/github",
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "docs/CHANGELOG.md"
+            }
+        ],
+        [
+            "@semantic-release/npm",
+            {
+                "npmPublish": false,
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                "assets": [
+                    "docs/CHANGELOG.md",
+                    "package.json",
+                    "package-lock.json"
+                ],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ]
+    ],
+    dryRun: false,
+    ci: false
 }
